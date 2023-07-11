f72eb2b06f5ec6e600d0c7c2b9ed545fb0a09df2
StellaGift <giftada151@gmail.com>
2023-07-11 12:25:48 +0000

    Let\'s talk about scripting with python interpreter...

---
+++ python/varAndstring.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#python script using strings and variable example
+
+import time
+
+print ("Let's talk about scripting with python!")
+time.sleep(0.9)
+
+print ('"quotes" can be used within strings.')
+time.sleep(1.1)
+
+print ("scripting" + " for" + " offensive" + " security.")
+time.sleep(1.3)
+
+print (' ')
+
+time.sleep(1.5)
+print ("Now let's take a look at variables..")
+time.sleep(1.4)
+
+greeting="Hello"
+name=input("Enter your name: ")
+period=('.')
+time.sleep(1.2)
+	
+print (greeting + ' ' + name + period)
+time.sleep(1.5)
+
+#example2
+python="Did you know that you can learn exploit Dev in"
+program=input("What are you learning? ")
+question="?"
+time.sleep(1.2)
+
+print (python + ' ' + program + question)
+print ("oh! well, you definitely can!")
+print (' ')
+time.sleep(0.8)
+print ("Happy scripting")
+print (' ')
