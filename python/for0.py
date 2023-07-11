9fd01a41489e45e5417bd5c73628714a7003e0de
StellaGift <giftada151@gmail.com>
2023-07-11 12:35:26 +0000

    iterates over a variable or object, excluding the last value

---
+++ python/for0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#for loop example
+#iterating over a variable or object, excluding the last value
+
+import time
+for i in range (10, 50):
+	time.sleep(0.5)
+	print ("{}".format(i))
